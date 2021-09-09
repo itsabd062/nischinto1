91ef23816343d769afccc4251e1830a464dc313e
Abdul Rehman <itsabd062@gmail.com>
2021-09-09 15:51:00 +0500

    almostfinal

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,7 +8,7 @@
  		$(".itembox").not('.' + value).hide('3000');
  		$('.itembox').filter('.' + value).show('3000');
  	  }
-   // active
+	   
  	  $(".list").removeClass('active');
  	  var $this = $(this);
  	  if (!$this.hasClass('active')) {
@@ -16,19 +16,6 @@
  	  }
    });
  	});
-// $(document).ready(function(){
-//     $('.list').click(function(){
-//       var value =$(this).attr('data-filter');
-//       if (value=='all'){
-//         $('.itembox').show('1000');
-//       }
-//       else{
-//         $('.itembox').not('.'+value).hide('1000');
-//         $('.itembox').filter('.'+value).show('1000');
-//       }
-//     })
-// 	$()
-//   })
 
 window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("mynavbar");
